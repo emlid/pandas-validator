73a2d82e089afd4b0772f4d7aea00ee353a18dce
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2018-08-03 18:00:56 +0300

    core/exceptions: Add BasicValidationError class

--- pandas_validator/core/exceptions.py
+++ pandas_validator/core/exceptions.py
@@ -1,10 +1,17 @@
 
-class ValidationError(Exception):
+class BasicValidationError(Exception):
     """An error while validating data."""
-    def __init__(self, message, error_type, column=None, rows=None):
-        super(ValidationError, self).__init__(message)
+    def __init__(self, message):
+        super(BasicValidationError, self).__init__(message)
 
         self.message = message
+
+
+class ValidationError(BasicValidationError):
+    """An error while validating data."""
+    def __init__(self, message, error_type, column, rows=None):
+        super(ValidationError, self).__init__(message)
+
         self.error_type = error_type
         self.column = column
         self.rows = rows
