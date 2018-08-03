fa13269e592745fa5993f0c5d93b63fcab7dc95a
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2018-08-03 16:55:19 +0300

    core/exceptions: Add properties to ValidationError class

--- pandas_validator/core/exceptions.py
+++ pandas_validator/core/exceptions.py
@@ -1,7 +1,10 @@
 
 class ValidationError(Exception):
     """An error while validating data."""
-    def __init__(self, message):
+    def __init__(self, message, error_type, column=None, rows=None):
         super(ValidationError, self).__init__(message)
 
         self.message = message
+        self.error_type = error_type
+        self.column = column
+        self.rows = rows
