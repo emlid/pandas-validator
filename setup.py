2fff7a6945901be14654715e95e4534871d51b6d
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2018-10-29 17:19:09 +0300

    setup.py: bump version to v0.7.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(BASE_PATH, 'README.rst')).read()
 CHANGES = open(os.path.join(BASE_PATH, 'CHANGES.rst')).read()
 
 __author__ = 'Masashi Shibata <contact@c-bata.link>'
-__version__ = '0.6.1'
+__version__ = '0.7.0'
 __license__ = 'MIT License'
 __author_email__ = 'contact@c-bata.link'
 __url__ = 'https://github.com/emlid/pandas-validator'
